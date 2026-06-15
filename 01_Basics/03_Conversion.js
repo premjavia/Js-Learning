ec102180e0a112fb26d871d19cb7137ba2887d6a
premjavia <premjavia77@gmail.com>
2026-06-15 14:19:02 +0000

    add string and number

--- 01_Basics/03_Conversion.js
+++ 01_Basics/03_Conversion.js
@@ -1,21 +1,32 @@
-let score = undefined
+// let score = undefined
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
 
+//---------------------------------------------------------
 //"33" -> 33
 // "33abc" -> NaN
 // true -> 1; false -> 0
+//---------------------------------------------------------
 
-let isLoggedIn = "Prem"
+// let isLoggedIn = "Prem"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
+//--------------------------------------------------------
 // 1 -> true; 0 -> false
 // "" -> false
 // "Prem" -> true
+//---------------------------------------------------------
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
