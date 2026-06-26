0a8d9ba4258315e59cbeb520e056abd42762aa60
premjavia <premjavia77@gmail.com>
2026-06-26 09:05:18 +0000

    maths

---
+++ 01_Basics/06_nums.js
@@ -0,0 +1,23 @@
+// const balance = new Number(600)
+// console.log(balance)
+// console.log(balance.toString())
+
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 23.8965
+// console.log(otherNumber.toFixed(2))
+// console.log(otherNumber.toPrecision(2))
+
+// const thirdNumber = 99.999
+// console.log(thirdNumber.toPrecision(1))
+
+// const fourthNumber = 234567
+// console.log(fourthNumber.toPrecision(2))
+
+const Hundreds = 1000000
+console.log(Hundreds.toLocaleString("en-IN"))
+
+console.log(Math.random())
+const max = 30
+const min = 25
+console.log(Math.floor((Math.random() * (max-min + 1) + min)))
